35a4347aed099cc70b352d5d036082198a769bde
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-02-28 21:56:32 +0100

    Update Chat.lua

--- Chat.lua
+++ Chat.lua
@@ -1,38 +1,12 @@
 --[[ Variables ]]--
 local Players = game:GetService("Players")
 local ServerScriptService = game:GetService("ServerScriptService")
-local DataStoreService = game:GetService("DataStoreService")
 script.Parent.Parent = ServerScriptService
 local commands = require(ServerScriptService["CGG Admin"].Commands)
 local configures = require(ServerScriptService["CGG Admin"].Configures)
-local prefix = "!"
+local prefix = configures.prefix
 
 --[[ Functions]]--
-
-local adminStore = DataStoreService:GetDataStore("Admins")
-local modStore = DataStoreService:GetDataStore("Mods")
-while true do
-	local success, updatedTable = pcall(function()
-		return adminStore:UpdateAsync(configures.admins)
-	end)
-	local success, updatedTable = pcall(function()
-		return modStore:UpdateAsync(configures.mods)
-	end)
-	task.wait(60)
-end
-
-Players.PlayerAdded:Connect(function(player)
-	if player.UserId == game.CreatorId then
-		configures.PermissionTable[player] = 5 -- For Owner
-	elseif player:GetRankInGroup(configures.groupId) > configures.modrank or table.find(configures.mods, player.UserId) then
-		configures.PermissionTable[player] = 4 -- For Mods
-	elseif player:GetRankInGroup(configures.groupId) > configures.adminrank or table.find(configures.admins, player.UserId) then
-		configures.PermissionTable[player] = 3 -- For Admins
-	else
-		configures.PermissionTable[player] = 1 -- No Perms!11! LLL
-	end
-end)
-
 function getArgs(splitmsg)
 	local inString = false
 	local newIndex = 1
@@ -62,6 +36,7 @@ function getArgs(splitmsg)
 end
 -- made by Cens_r
 Players.PlayerAdded:Connect(function(player)
+	commands.refreshranks("pp", player)
 	player.Chatted:Connect(function(msg)
 		if string.sub(msg,1,1) == prefix then
 			local playerId = player.UserId
