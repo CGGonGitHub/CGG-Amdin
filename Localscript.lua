35587bc3a5d386973a5cd19574bcc632766dc291
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2023-03-01 02:13:10 +0100

    Update Localscript.lua

--- Localscript.lua
+++ Localscript.lua
@@ -1,7 +1,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Event = ReplicatedStorage.AdminBarEvent
 local TextBox = script.Parent
-local TextLabel = script.Parent.Parent.TextLabel
+local TextLabel = script.Parent.Parent
 local commands = {
 	"kill";
 	"anchor";
@@ -28,6 +28,19 @@ local commands = {
 	"unfreeze";
 	"rejoin"
 }
+
+TextBox:GetPropertyChangedSignal("Text"):Connect(function()
+	local text = TextBox.Text
+	if string.len(text) < 2 then
+		return
+	else
+		for i, v in commands do
+			if string.match(v, text) then
+				TextLabel.Text = v
+			end
+		end		
+	end
+end)
 
 TextBox.FocusLost:Connect(function(enterPressed, inputThatCausedFocusLost)
 	if enterPressed then
